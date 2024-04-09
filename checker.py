741d980a38f62c6a83d516cc0e793b9ad80711e0
matthcol <matthias.colin@gmail.com>
2024-04-09 14:22:35 +0200

    first commit

---
+++ checker.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def is_magic(square: np.ndarray) -> bool:
+    pass
+
+# n (n² + 1) / 2
+def magic_sum(n: int) -> int:
+    pass
+
+def is_magic_rows(square: np.ndarray, n: int, ms: int) -> bool:
+    pass
+
+def is_magic_columns(square: np.ndarray, n: int, ms: int) -> bool:
+    pass
+
+def is_magic_diagonals(square: np.ndarray, n: int, ms: int) -> bool:
+    pass
+
+def all_numbers_used(square: np.ndarray, n: int) -> bool:
+    pass
---
+++ data.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+square3_ok = np.array([
+    [2, 7 , 6],
+    [9, 5 , 1],
+    [4, 3 , 8],
+])
